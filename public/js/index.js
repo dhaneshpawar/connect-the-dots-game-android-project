faa3b581cb2a2236d417168b65e0f7be7a62434b
Dhanesh Pawar <dhaneshpawar.pro@gmail.com>
2020-04-23 16:44:46 +0530

    firebase initited

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function()
+{
+
+  $("#popup1").hide();  
+  
+  $("#floatLogo").fadeIn(1000); 
+  
+  $("#moreinfo").fadeIn(1000);
+
+  $("#sign-in-div").fadeIn(1000);  
+  
+  $("#tagline").hide(1000);  
+  $("#tagline").show(1000);  
+  
+  for(var i=0;i<1000;i++)
+  {
+    $("#tagline").fadeOut(1000);  
+    $("#tagline").fadeIn(1000);    
+  }
+ 
+  $( "#sign-in" ).click(function() 
+  {
+      $(this).attr("disabled", "disabled");
+   });
+ 
+   $( "#sigingooglebutton" ).click(function() 
+   {
+    $("#popup1").show(1000);  
+  });
+  
+});  
